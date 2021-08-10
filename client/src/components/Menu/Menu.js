e59f0db613b5e36df1cc4835a4cf027ee78cdbd5
Adam Underhill <adameunderhill@gmail.com>
2021-08-10 00:00:24 -0700

    css updates

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -7,26 +7,36 @@ const Menu = ({ open, ...props }) => {
   const tabIndex = isHidden ? 0 : -1;
 
   return (
+    
     <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
       <a href="/" tabIndex={tabIndex}>
-        <span aria-hidden="true"> 🏠</span>
+        <span aria-hidden="true"> </span>
         Home
       </a>
       <a href="/about" tabIndex={tabIndex}>
-        <span aria-hidden="true"> 💡 </span>
+        <span aria-hidden="true">  </span>
         About us
       </a>
       <a href="/login" tabIndex={tabIndex}>
-        <span aria-hidden="true"> s🗂 </span>
+        <span aria-hidden="true">  </span>
         Login
       </a>
       <a href="/register" tabIndex={tabIndex}>
-        <span aria-hidden="true"> 🗃 </span>
+        <span aria-hidden="true">  </span>
         Register
       </a>
       <a href="/gallery" tabIndex={tabIndex}>
-        <span aria-hidden="true"> 🎨 </span>
+        <span aria-hidden="true">  </span>
         Gallery
+        
+      </a>
+      <a href="/ArtistRequests" tabIndex={tabIndex}>
+        <span aria-hidden="true">  </span>
+        Commission Requests
+      </a>
+      <a href="/ArtistAccount" tabIndex={tabIndex}>
+        <span aria-hidden="true">  </span>
+        My Artist Account
       </a>
     </StyledMenu>
   );
